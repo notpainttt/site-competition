5768d6e530bb27d1a0727da1fe94e90545ba0c3c
Painttt <125980326+notpainttt@users.noreply.github.com>
2025-11-21 15:53:02 +0700

    Delete template directory

--- template/footer.php
+++
@@ -1,9 +0,0 @@
-    <!-- Footer -->
-    <footer class="footer">
-        <p>Love coding, copyright 2024</p>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-</body>
-</html>
